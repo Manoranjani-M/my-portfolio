519fc2e351b8f54c228ab0279f16b995c5f29520
Manoranjani M <161202128+Manoranjani-M@users.noreply.github.com>
2024-07-27 15:41:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
